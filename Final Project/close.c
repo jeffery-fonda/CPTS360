670c51ca3f0ffa50367fec48a592566e7401f989
Jeffery <jeffery.fonda@email.wsu.edu>
2018-04-13 11:43:36 -0700

    Cleaned up code

--- Final Project/close.c
+++ Final Project/close.c
@@ -1,60 +1,60 @@
 //Takes in a file descriptor
 void close_file(int fd)
 {
-								int i;
-								OFT *fp; //OpenFileTable file pointer
+                int i;
+                OFT *fp; //OpenFileTable file pointer
 
-								//Checks if the fd is possible
-								if(fd < 0 || fd >= NFD)
-								{
-																printf("ERROR: fd out of range\n");
-																return;
-								}
+                //Checks if the fd is possible
+                if(fd < 0 || fd >= NFD)
+                {
+                                printf("ERROR: fd out of range\n");
+                                return;
+                }
 
-								//Checks if the fd is on the OpenFileTable
-								//if it isn't, this fd must not be open
-								for(i = 0; i < NOFT; i++)
-								{
-																fp = &OpenFileTable[i];
+                //Checks if the fd is on the OpenFileTable
+                //if it isn't, this fd must not be open
+                for(i = 0; i < NOFT; i++)
+                {
+                                fp = &OpenFileTable[i];
 
-																//breaks if it finds the fd on the OpenFileTable
-																if(fp->inodeptr == running->fd[fd]->inodeptr)
-																								break;
+                                //breaks if it finds the fd on the OpenFileTable
+                                if(fp->inodeptr == running->fd[fd]->inodeptr)
+                                                break;
 
-																if(i == NOFT - 1)
-																{
-																								printf("ERROR: File not in OpenFileTable\n");
-																								return;
-																}
-								}
+                                if(i == NOFT - 1)
+                                {
+                                                printf("ERROR: File not in OpenFileTable\n");
+                                                return;
+                                }
+                }
 
-								//sets fp, an OFT, to the correct fd from the running proccess
-								fp = running->fd[fd];
-								running->fd[fd] = NULL;
+                //sets fp, an OFT, to the correct fd from the running proccess
+                fp = running->fd[fd];
+                running->fd[fd] = NULL;
 
-								//Ensures that the minodes are running as should be
-								fp->refCount--;
+                //Ensures that the minodes are running as should be
+                fp->refCount--;
 
-								if(fp->refCount == 0)
-																iput(fp->inodeptr);
+                if(fp->refCount == 0)
+                                iput(fp->inodeptr);
 
-								return;
+                return;
 }
 
 //Calls close file for the given fd
 void my_close(char *path)
 {
-								int fd;
+                int fd;
 
-								//check for missing path
-								if(!path)
-								{
-																printf("ERROR: no file name given!\n");
-																return;
-								}
+                //check for missing path
+                if(!path)
+                {
+                                printf("ERROR: no file name given!\n");
+                                return;
+                }
 
-								//converts path to fd
-								fd = atoi(path);
-								close_file(fd);
-								return;
+                //converts path to fd
+                fd = atoi(path);
+                close_file(fd);
+                return;
 }
--- Final Project/main.c
+++ Final Project/main.c
@@ -41,7 +41,7 @@
    File permission checking
 
    Simple EXT2 File System:
- |	 0		  1		   2		 3		  4	 | 5................49 |	50...............|
+ |   0      1       2     3      4   | 5................49 |  50...............|
  | Boot | Super | GD | BMAP | IMAP | Inodes Blocks...... | data blocks...... |
 
    Each block group contains the following blocks:
@@ -79,96 +79,96 @@
 
 void menu() //displays the available commands
 {
-								printf("Level 1 COMMANDS:\n\n");
-								printf("	mkdir\t\trmdir\t\tls\t\tcd\n");
-								printf("	cd\t\tpwd\t\tcreat\t\tlink\n");
-								printf("	unlink\t\tsymlink\t\tstat\t\tchmod\n");
-								printf("	touch\t\tquit\t\tmenu\n\n");
-								printf("Level 2 COMMANDS:\n\n");
-								printf("	open\t\tclose\t\tread\t\twrite\n");
-								printf("	lseek\t\tcat\t\tcp\t\tmv\n");
+                printf("Level 1 COMMANDS:\n\n");
+                printf("  mkdir\t\trmdir\t\tls\t\tcd\n");
+                printf("  cd\t\tpwd\t\tcreat\t\tlink\n");
+                printf("  unlink\t\tsymlink\t\tstat\t\tchmod\n");
+                printf("  touch\t\tquit\t\tmenu\n\n");
+                printf("Level 2 COMMANDS:\n\n");
+                printf("  open\t\tclose\t\tread\t\twrite\n");
+                printf("  lseek\t\tcat\t\tcp\t\tmv\n");
 }
 
 int main()
 {
-								char *commands[23] = {"mkdir", "rmdir", "ls", "cd", "pwd", "creat", "link", "unlink", "symlink", "stat", "chmod", "touch", "quit", "menu", "open", "close", "pfd", "write", "cat", "read", "cp", "mv", "lseek"};
-								char device_name[64], input[128] = "";
-								char command[64], pathname[64] = "";
+                char *commands[23] = {"mkdir", "rmdir", "ls", "cd", "pwd", "creat", "link", "unlink", "symlink", "stat", "chmod", "touch", "quit", "menu", "open", "close", "pfd", "write", "cat", "read", "cp", "mv", "lseek"};
+                char device_name[64], input[128] = "";
+                char command[64], pathname[64] = "";
 
-								int (*functions[23]) (char path[]);
-								int i;
+                int (*functions[23]) (char path[]);
+                int i;
 
-								//set the function at index n to the name of the function in the specific file
-								//program then goes through the functions array until it find the command that has been inputted
-								//once the command has been found, it executes the defined function with the correct passed through values
-								//all functions take at least one additional variable, path
-								//path is used because file_name is obnoxious to type over and over again
-								functions[0] = make_dir; //works
-								functions[1] = remove_dir; //works
-								functions[2] = ls; //works
-								functions[3] = cd; //works
+                //set the function at index n to the name of the function in the specific file
+                //program then goes through the functions array until it find the command that has been inputted
+                //once the command has been found, it executes the defined function with the correct passed through values
+                //all functions take at least one additional variable, path
+                //path is used because file_name is obnoxious to type over and over again
+                functions[0] = make_dir; //works
+                functions[1] = remove_dir; //works
+                functions[2] = ls; //works
+                functions[3] = cd; //works
 
-								functions[4] = my_pwd; //works
-								functions[5] = creat_file; //works
-								functions[6] = my_link; //works
-								functions[7] = my_unlink; //works
+                functions[4] = my_pwd; //works
+                functions[5] = creat_file; //works
+                functions[6] = my_link; //works
+                functions[7] = my_unlink; //works
 
-								functions[8] = my_symlink; //works
-								functions[9] = my_stat; //works
-								functions[10] = chmod_file; //kind of works
-								functions[11] = touch_file; //works
-								functions[12] = quit; //works
-								functions[13] = menu; //works
-								functions[14] = open_file; //works
-								functions[15] = my_close; //works
-								functions[16] = my_pfd; //works
-								functions[17] = do_write; //works
-								functions[18] = my_cat; //works
-								functions[19] = read_file; //works
-								functions[20] = cp_file; //segmentation fault
-								functions[21] = mv_file; //the file dissapears
-								functions[22] = my_lseek; //works
+                functions[8] = my_symlink; //works
+                functions[9] = my_stat; //works
+                functions[10] = chmod_file; //kind of works
+                functions[11] = touch_file; //works
+                functions[12] = quit; //works
+                functions[13] = menu; //works
+                functions[14] = open_file; //works
+                functions[15] = my_close; //works
+                functions[16] = my_pfd; //works
+                functions[17] = do_write; //works
+                functions[18] = my_cat; //works
+                functions[19] = read_file; //works
+                functions[20] = cp_file; //segmentation fault
+                functions[21] = mv_file; //the file dissapears
+                functions[22] = my_lseek; //works
 
 
-								printf("Enter device name: "); //prompt user for device
-								scanf("%s", device_name); //scan in device name
+                printf("Enter device name: "); //prompt user for device
+                scanf("%s", device_name); //scan in device name
 
-								//mount device
-								init();
-								mount_root(device_name);
+                //mount device
+                init();
+                mount_root(device_name);
 
-								getchar();
-								while(1) //run program forever until quit
-								{
-																printf("jfonda@ubuntu:~$ "); //terminal look
-																fgets(input, 128, stdin);
+                getchar();
+                while(1) //run program forever until quit
+                {
+                                printf("jfonda@ubuntu:~$ "); //terminal look
+                                fgets(input, 128, stdin);
 
-																//remove \r at end of input
-																input[strlen(input) - 1] = 0;
-																if(input[0] == 0)
-																								continue;
+                                //remove \r at end of input
+                                input[strlen(input) - 1] = 0;
+                                if(input[0] == 0)
+                                                continue;
 
-																//split up the input into the variables
-																sscanf(input, "%s %s %s", command, pathname, third);
+                                //split up the input into the variables
+                                sscanf(input, "%s %s %s", command, pathname, third);
 
-																for(i = 0; i < 23; i++)
-																{
-																								//commands is list of commands, command is the inputted command
-																								if(!strcmp(commands[i], command))
-																								{
-																																(*functions[i])(pathname); //pathname is just file name
-																																break; //break once command is found and executed
-																								}
-																}
+                                for(i = 0; i < 23; i++)
+                                {
+                                                //commands is list of commands, command is the inputted command
+                                                if(!strcmp(commands[i], command))
+                                                {
+                                                                (*functions[i])(pathname); //pathname is just file name
+                                                                break; //break once command is found and executed
+                                                }
+                                }
 
-																if(i == 23) //if no command has been found, then it doesn't exist
-																								printf("Error: Invalid command!\n");
+                                if(i == 23) //if no command has been found, then it doesn't exist
+                                                printf("Error: Invalid command!\n");
 
-																//reset variables
-																strcpy(pathname, "");
-																strcpy(command, "");
-																strcpy(input, "");
-																strcpy(third, "");
-								}
-								return 0;
+                                //reset variables
+                                strcpy(pathname, "");
+                                strcpy(command, "");
+                                strcpy(input, "");
+                                strcpy(third, "");
+                }
+                return 0;
 }
